6ff8e1069293896ea130d97e3738e2c2c2a2f628
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-09-05 16:10:10 +0200

    paperclip gem added & model and controller review added and link with book and user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160905131756) do
+ActiveRecord::Schema.define(version: 20160905135147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,22 @@ ActiveRecord::Schema.define(version: 20160905131756) do
     t.date     "release_date"
     t.date     "release_pocket_date"
     t.integer  "user_id"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+    t.string   "book_img_file_name"
+    t.string   "book_img_content_type"
+    t.integer  "book_img_file_size"
+    t.datetime "book_img_updated_at"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.text     "resume"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "book_id"
   end
 
   create_table "users", force: :cascade do |t|
